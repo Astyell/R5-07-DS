e61c0408cbd3e23f19bedd30a19b5cae6d6b1717
Astyell <90407015+Astyell@users.noreply.github.com>
2024-11-20 14:33:30 +0100

    Create Q3.java

---
+++ Q3.java
@@ -0,0 +1,31 @@
+public class LigneEtoile {
+    public static void main(String[] args) 
+    {
+        int nombre = 0;
+        String chaine = "";
+      
+        // Vérification du nombre d'arguments
+    		if (args.length != 1) 
+    		{
+    			System.err.println("Erreur, utilisez le format suivant : java Q3 3");
+    			System.exit(0);
+    		}
+  
+        try 
+  			{
+  				nombre = Integer.parseInt(args[0]);
+  			}
+  			catch (Exception e) 
+  			{
+  				System.err.println("L'argument doit être un nombre");
+  				System. exit(0);
+  			}
+      
+        for (int i = 0; i < nombre; i++)
+        {
+            chaine += "*";
+        }
+
+        System.out.println(chaine);     
+    }
+}
